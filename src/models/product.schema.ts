8e33f479c209d86655cb45a5bd5b5390ad805511
Thomas Johnson <tjohnson1106@gmail.com>
2019-05-03 15:52:53 -0400

    created order schema model

---
+++ src/models/product.schema.ts
@@ -0,0 +1,18 @@
+import * as mongoose from "mongoose";
+
+export const ProductSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  title: {
+    title: String,
+    description: String,
+    image: String,
+    price: String,
+    created: {
+      type: Date,
+      default: Date.now
+    }
+  }
+});
