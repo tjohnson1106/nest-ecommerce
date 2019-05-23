649fb531ed44a8589102bdc39e7ee9385310f07e
Thomas Johnson <tjohnson1106@gmail.com>
2019-05-23 15:40:21 -0400

    created passport strategyt

--- src/shared/user.service.ts
+++ src/shared/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable, Inject } from "@nestjs/common";
+import { Injectable, Inject, HttpException, HttpStatus } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import * as bcrypt from "bcrypt";
+
 import { User } from "../types/user";
+import { RegisterDTO, LoginDTO } from "../auth/auth.dto";
 
 @Injectable()
 export class UserService {
@@ -11,7 +14,36 @@ export class UserService {
     return user.depopulate("password");
   }
 
-  create() {}
+  async create(userDTO: RegisterDTO) {
+    console.log(userDTO);
 
-  findByLogin() {}
+    const { username } = userDTO;
+    const user = await this.userModel.findOne({ username });
+    if (user) {
+      throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
+    }
+
+    const createdUser = new this.userModel(userDTO);
+    await createdUser.save();
+    return this.sanitizeUser(createdUser);
+  }
+
+  async findByLogin(userDTO: LoginDTO) {
+    const { username, password } = userDTO;
+    const user = await this.userModel.findOne({ username });
+    if (!user) {
+      throw new HttpException("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+      return this.sanitizeUser(user);
+    } else {
+      throw new HttpException("Invalid Credentials", HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  async findByPayload(payload: any) {
+    const { username } = payload;
+    return await this.userModel.findOne({ username });
+  }
 }
